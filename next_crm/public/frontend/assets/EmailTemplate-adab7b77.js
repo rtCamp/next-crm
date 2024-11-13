b22e407313a720ca621a7d2d0aa53f6ceec38281
Aryan Kaushik <aryankaushik2023@gmail.com>
2024-11-13 18:10:21 +0000

    fix: remove ignored files

--- next_crm/public/frontend/assets/EmailTemplate-adab7b77.js
+++
@@ -1,2 +0,0 @@
-import{_ as t,b as s,c as a,e}from"./index-8ca2d158.js";const o={},c=e("h1",null,"Email Templates",-1),l=e("p",null,"Here is a list of email templates",-1),n=[c,l];function _(r,i){return s(),a("div",null,n)}const d=t(o,[["render",_]]);export{d as default};
-//# sourceMappingURL=EmailTemplate-adab7b77.js.map
