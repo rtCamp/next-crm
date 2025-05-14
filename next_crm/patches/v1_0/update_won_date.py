30c243e061f065b6d014356d51cf685bc3045f7d
Niraj Gautam <nirajgautam196@gmail.com>
2025-05-14 17:00:02 +0530

    patch: Add patch to update won date as per the status log

---
+++ next_crm/patches/v1_0/update_won_date.py
@@ -0,0 +1,28 @@
+def execute():
+    from frappe import db, get_all
+    from frappe.utils import getdate, update_progress_bar
+
+    opportunity_list = get_all(
+        "Opportunity",
+        filters={"custom_won_date": ["is", "not set"], "status": "Won"},
+        pluck="name",
+    )
+    count = len(opportunity_list)
+
+    for index, opportunity in enumerate(opportunity_list):
+        d = db.get_value(
+            "CRM Status Change Log",
+            {"to": "Won", "parent": opportunity, "to_date": ["is", "set"]},
+            "to_date",
+        )
+        if not d:
+            update_progress_bar("Updating Won Date", index, count)
+            continue
+        db.set_value(
+            "Opportunity",
+            opportunity,
+            "custom_won_date",
+            getdate(d),
+            update_modified=False,
+        )
+        update_progress_bar("Updating Won Date", index, count)
