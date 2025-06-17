59228aa68d0ea2a859e49f4900113b7a89f25708
Aryan Kaushik <aryankaushik2023@gmail.com>
2025-06-17 08:04:59 +0000

    send comment notifications for only CRM concerned docs

--- next_crm/doc_events/comment.py
+++ next_crm/doc_events/comment.py
@@ -2,4 +2,14 @@ from next_crm.api.comment import notify_mentions
 
 
 def on_update(doc, method=None):
-    notify_mentions(doc)
+    allowed_doctypes = [
+        "ToDo",
+        "Opportunity",
+        "Lead",
+        "Prospect",
+        "Customer",
+        "Address",
+        "Contact",
+    ]
+    if doc.reference_doctype in allowed_doctypes:
+        notify_mentions(doc)
