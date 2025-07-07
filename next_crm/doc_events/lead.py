ae2a07a2a88551c5e1d8e89bbdba05b8b12ca81c
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2025-07-07 12:14:30 +0530

    Update on_trash hook for opportunity and lead to delete attached files in crm notes

--- next_crm/doc_events/lead.py
+++ next_crm/doc_events/lead.py
@@ -1,3 +1,10 @@
+from next_crm.doc_events.utils import delete_attachments_from_crm_notes
+
+
 def on_update(doc, method=None):
     if not doc.title and doc.name:
         doc.db_set("title", doc.name)
+
+
+def on_trash(doc, method=None):
+    delete_attachments_from_crm_notes(doc.doctype, doc.name)
